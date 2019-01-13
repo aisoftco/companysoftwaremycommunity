257bf91284eb77cb228da626359c263efe60ffaf
Danilo Grisalez <mdgrisalez@misena.edu.co>
2019-01-13 08:52:00 -0500

    Servicios y portafolio listo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ServiceComponent } from './service/service.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServiceComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule
